5c96685293516bbabee80b7f8ba74c8d06f2e069
beni <benjamin.bajralija@uzh.ch>
2023-04-23 21:08:59 +0200

    Feature: Edit the design of Create Game Design & Lobby Pages. close #69

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -1,11 +1,12 @@
 import { useParams, useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
-import "styles/views/Home.scss";
+import "styles/views/Lobby.scss";
 import { useEffect, useState } from "react";
 import * as SockJS from "sockjs-client";
 import * as Stomp from "stompjs";
 import Game from "models/Game";
 import { getDomain } from "helpers/getDomain";
+import { Button } from "components/ui/Button";
 
 const Lobby = () => {
   const gameId = useParams().gameId;
@@ -56,7 +57,7 @@ const Lobby = () => {
   });
 
   //useEffect(() => {}, [game]);
-
+  /*
   let content = (
     <div className="profile overview">
       <div>Game id: {gameId}</div>
@@ -68,18 +69,66 @@ const Lobby = () => {
       )}
     </div>
   );
-
+*/
   return (
-    <BaseContainer className="home container">
-      <h2>Lobby for Game {gameId} </h2>
-      {content}
-      {/* <div className="login container">
-        <div className="login button-container">
-          <Button width="100%" onClick={() => startGame(stompClient)}>
-            Start Game
+    <BaseContainer>
+      <div className="createGame container">
+        <div className="createGame title-container">
+          <h1 className="createGame title">Game Lobby</h1>
+        </div>
+        <div className="createGame subtitle-spectator-container">
+          <h2 className="createGame subtitle">Players</h2>
+          {/*<h4 className="createGame spectators">
+            Spectators:
+            <span className="spectators-number">
+              TODO: function to get number of spectators 0
+            </span>
+          </h4>*/}
+        </div>
+        <div className="createGame player-container">
+          <h4 className="createGame name">
+            {sessionStorage.getItem("username")}
+          </h4>
+          <h4 className="createGame host">host</h4>
+        </div>
+        <div
+          className="createGame player-container"
+          style={{ marginBottom: "2em" }}>
+          <h4 className="createGame name">{game.guestStatus} ...</h4>
+          <Button className="createGame kick-player" disabled="true">
+            kick player
           </Button>
         </div>
-      </div> */}
+        <div className="createGame subtitle-spectator-container">
+          <h2 className="createGame subtitle">Game Informations</h2>
+        </div>
+        <div className="createGame player-container">
+          <h4 className="createGame name">Game ID</h4>
+          <h4 className="createGame host">{game.gameId}</h4>
+        </div>
+        {/* TODO: DELETE THE NEXT PLAYER-CONTAINERS */}
+        <div className="createGame player-container">
+          <h4 className="createGame name">Host Status</h4>
+          <h4 className="createGame host">{game.hostStatus}</h4>
+        </div>
+        <div className="createGame player-container">
+          <h4 className="createGame name">Guest Status</h4>
+          <h4 className="createGame host">{game.guestStatus}</h4>
+        </div>
+        <div className="createGame player-container">
+          <h4 className="createGame name">Game Status</h4>
+          <h4 className="createGame host">{game.gameStatus}</h4>
+        </div>
+        <div className="createGame player-container">
+          {game.hostStatus === "CONNECTED" &&
+            game.guestStatus === "CONNECTED" && (
+              <h2 className="createGame subtitle">The game will start soon.</h2>
+            )}
+          {game.guestStatus === "WAITING" && (
+            <h2 className="createGame subtitle">Waiting for an opponent</h2>
+          )}
+        </div>
+      </div>
     </BaseContainer>
   );
 };
