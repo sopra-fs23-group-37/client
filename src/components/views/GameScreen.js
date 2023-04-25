74c3b3425f11c6bd6f6081eb69b011527afc59dc
Carol Ernst <carolbarbara.ernst@uzh.ch>
2023-04-25 16:53:37 +0200

    cleaned up GameScreen

--- src/components/views/GameScreen.js
+++ src/components/views/GameScreen.js
@@ -19,28 +19,28 @@ const GameScreen = () => {
   const [gameStarted, setGameStarted] = useState(false);
   const [round, setRound] = useState(null);
   // end of round contains points for the round and total points
-  // const [endOfRound, setEndOfRound] = useState(false);
-  // // end of game contains total points and winner
-  // const [endOfGame, setEndOfGame] = useState(false);
-  // // contains all cards from the player and on the discard
-  // const [playerCards, setPlayerCards] = useState(null);
-  // const [playerDiscards, setPlayerDiscardCards] = useState(null);
+  const [endOfRound, setEndOfRound] = useState(false);
+  // end of game contains total points and winner
+  const [endOfGame, setEndOfGame] = useState(false);
+  // contains all cards from the player and on the discard
+  const [playerCards, setPlayerCards] = useState(null);
+  const [playerDiscards, setPlayerDiscardCards] = useState(null);
   // contains number of cards of the opponent
-  // const [opponentCards, setOpponentCards] = useState(null);
-  // const [opponentDiscard, setOpponentDiscard] = useState(null);
-  // // contains the cards on the table as array
-  // const [tableCards, setTableCards] = useState(null);
-  // // whether or not there are still cards in the deck
-  // const [deckCards, setDeckCards] = useState(true);
-  // // contains id of the player who's turn it is
-  // const [playerTurn, setPlayerTurn] = useState(null);
-  // // true if the opponent has left
-  // const [opponentLeft, setOpponentLeft] = useState(false);
+  const [opponentCards, setOpponentCards] = useState(null);
+  const [opponentDiscard, setOpponentDiscard] = useState(null);
+  // contains the cards on the table as array
+  const [tableCards, setTableCards] = useState(null);
+  // whether or not there are still cards in the deck
+  const [deckCards, setDeckCards] = useState(true);
+  // contains id of the player who's turn it is
+  const [playerTurn, setPlayerTurn] = useState(null);
+  // true if the opponent has left
+  const [opponentLeft, setOpponentLeft] = useState(false);
   // set reason for why the player has left (e.g. unexpected disconnect, surrender)
-  // const [opponentLeftReason, setOpponentLeftReason] = useState(null);
+  const [opponentLeftReason, setOpponentLeftReason] = useState(null);
 
-  // these datapoints are set by the player when playing to form the move
-  // const [selectedCard, setSelectedCard] = useState(null);
+  //these datapoints are set by the player when playing to form the move
+  const [selectedCard, setSelectedCard] = useState(null);
 
   const history = useHistory();
 
@@ -49,27 +49,6 @@ const GameScreen = () => {
     console.log("game data received: ", data);
     setGame(new Game(data));
   };
-
-  // const printStuff = () => {
-  //   console.log("GuestStatus: ", game.guestStatus);
-  //   console.log("HostStatus: ", game.hostStatus);
-  // };
-
-  // const fetchGame = async () => {
-  //   try {
-  //     const response = await api.get("/games/" + gameId);
-  //     console.log("REST Response:", response);
-  //     setGame(new Game(response.data));
-  //   } catch (error) {
-  //     console.error(
-  //       `Something went wrong while fetching the game: \n${handleError(error)}`
-  //     );
-  //     console.error("Details:", error);
-  //     alert(
-  //       "Something went wrong while fetching the game! See the console for details."
-  //     );
-  //   }
-  // };
 
   const updateRound = (data) => {
     console.log("round update received:", data);
@@ -80,14 +59,6 @@ const GameScreen = () => {
     // check that the websocket remains connected and add the updateGame function
     console.log("websocket status:", sockClient.isConnected());
   };
-
-  // //use this function to change values!
-  // const setTestingValues = () => {
-  //   setEndOfRound(false);
-  //   setEndOfGame(true);
-  //   //setPlayerCards(test);
-  //   // continue here afterwards.
-  // };
 
   const startGame = () => {
     // add subscriptions
@@ -105,11 +76,6 @@ const GameScreen = () => {
     console.log("Use Effect started");
     checkWebsocket();
 
-    // // fetch the game data if it is not there yet
-    // if (!game) {
-    //   fetchGame();
-    // }
-
     // if the game has not started yet, start the game
     if (!gameStarted) {
       startGame();
@@ -125,56 +91,54 @@ const GameScreen = () => {
       sockClient.removeMessageFunctions();
     });
 
-    // setTestingValues();
-
     return () => {
       console.log("Component is unmounting");
       unlisten();
     };
   });
 
-  // let playerHand = (
-  //   <div className="card-container">
-  //     {/* Placeholder for player hand */}
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //   </div>
-  // );
+  let playerHand = (
+    <div className="card-container">
+      {/* Placeholder for player hand */}
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
 
-  // let opponentHand = (
-  //   <div className="card-container">
-  //     {/* Placeholder for opponent hand */}
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //   </div>
-  // );
+  let opponentHand = (
+    <div className="card-container">
+      {/* Placeholder for opponent hand */}
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
 
-  // let deck = (
-  //   <div className="card-container">
-  //     {/* Placeholder for deck */}
-  //     <div className="card back"></div>
-  //   </div>
-  // );
+  let deck = (
+    <div className="card-container">
+      {/* Placeholder for deck */}
+      <div className="card back"></div>
+    </div>
+  );
 
-  // let table = (
-  //   <div className="card-container">
-  //     {/* Placeholder for table cards */}
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //     <div className="card"></div>
-  //   </div>
-  // );
+  let table = (
+    <div className="card-container">
+      {/* Placeholder for table cards */}
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
 
-  // let content = (
-  //   <div className="game-container">
-  //     <div className="player-hand">{playerHand}</div>
-  //     <div className="opponent-hand">{opponentHand}</div>
-  //     <div className="table-cards">{table}</div>
-  //     <div className="deck">{deck}</div>
-  //   </div>
-  // );
+  let content = (
+    <div className="game-container">
+      <div className="player-hand">{playerHand}</div>
+      <div className="opponent-hand">{opponentHand}</div>
+      <div className="table-cards">{table}</div>
+      <div className="deck">{deck}</div>
+    </div>
+  );
 
   /*
   TODO: Timons code gibt Errors und lässt nicht rendern
