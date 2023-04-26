af9a9f8672e66bf801f5761f0d887fa4c063be27
Carol Ernst <carolbarbara.ernst@uzh.ch>
2023-04-26 09:40:47 +0200

    fixing issues resulting from merge

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -13,13 +13,8 @@ const Lobby = () => {
   const playerId = parseInt(sessionStorage.getItem("userId"));
   const [goingToGame, setGoingToGame] = useState(false);
 
-  const updateLobby = async (data) => {
+  const updateLobby = (data) => {
     setGame(new Game(data));
-    if (data.gameStatus === "CONNECTED") {
-      setGoingToGame(true);
-      await delay(1000);
-      history.push(`/game/play/${gameId}`);
-    }
   };
 
   const connectAndJoin = () => {
@@ -32,11 +27,21 @@ const Lobby = () => {
     }
   };
 
+  const goToGame = async () => {
+    if (game.gameStatus === "CONNECTED") {
+      setGoingToGame(true);
+      await delay(1000);
+      history.push(`/game/play/${gameId}`);
+    }
+  };
+
   useEffect(() => {
     console.log("Use Effect started");
     connectAndJoin();
 
     console.log("Current Lobby data: ", game);
+
+    goToGame();
 
     const unlisten = history.listen(() => {
       console.log("is the user going to the game? ", goingToGame);
